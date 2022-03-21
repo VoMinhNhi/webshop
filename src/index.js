f00056c0ef791736449931692d627c99c67f25b2
minhnhi <vminhnhiv@gmail.com>
2022-03-22 02:29:59 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+
 const port = 3000
+
+// HTTP engine
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
